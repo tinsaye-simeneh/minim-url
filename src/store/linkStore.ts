8fa32624ee0ec2a67d4933ff931ef187728f2f21
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-16 21:47:06 +0300

    refactor: remove content

--- src/store/linkStore.ts
+++ src/store/linkStore.ts
@@ -1,15 +1,1 @@
-import { create } from "zustand";
-import { Link } from "../types/models";
 
-interface LinksState {
-  links: Link[];
-  addLink: (link: Link) => void;
-  removeLink: (id: string) => void;
-}
-
-export const useLinksStore = create<LinksState>((set) => ({
-  links: [] as Link[],
-  addLink: (link: Link) => set((state) => ({ links: [...state.links, link] })),
-  removeLink: (id) =>
-    set((state) => ({ links: state.links.filter((link) => link.id !== id) })),
-}));
