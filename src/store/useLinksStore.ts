75df5686a07d2d6aef92388f3f99151b36cc8df8
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-16 20:53:17 +0300

    chore: zustand store setup

---
+++ src/store/useLinksStore.ts
@@ -0,0 +1,21 @@
+// src/store/useLinksStore.ts
+import { create } from "zustand";
+
+interface Link {
+  id: string;
+  original: string;
+  short: string;
+}
+
+interface LinksState {
+  links: Link[];
+  addLink: (link: Link) => void;
+  removeLink: (id: string) => void;
+}
+
+export const useLinksStore = create<LinksState>((set) => ({
+  links: [],
+  addLink: (link) => set((state) => ({ links: [...state.links, link] })),
+  removeLink: (id) =>
+    set((state) => ({ links: state.links.filter((link) => link.id !== id) })),
+}));
