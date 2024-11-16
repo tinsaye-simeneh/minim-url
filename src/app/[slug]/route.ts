e4593dcaadb033d07ebea7ef347bbe062fdd4d34
Tinsaye S. Dessie <64516393+tinsaye-simeneh@users.noreply.github.com>
2024-11-16 22:29:29 +0300

    Update route.ts

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -3,18 +3,18 @@ import { supabase } from "@/utils/supabaseClient";
 
 export async function GET(
   req: Request,
-  { params }: { params: { slug: string } }
+  { params }: { params: Record<string, string> }
 ) {
-  const { slug } = params;
+  const { slug } = params; // 'slug' is a key in 'params'
 
   const { data, error } = await supabase
-    .from("links")
-    .select("original_url")
-    .eq("short_url", slug)
+    .from('links')
+    .select('original_url')
+    .eq('short_url', slug)
     .single();
 
   if (error || !data) {
-    return NextResponse.json({ message: "Link not found" }, { status: 404 });
+    return NextResponse.json({ message: 'Link not found' }, { status: 404 });
   }
 
   return NextResponse.redirect(data.original_url, 301);
