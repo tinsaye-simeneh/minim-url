6c0b635886a1a0dc726a084deb8f423f64f616b2
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2024-11-18 08:41:19 +0300

    feat: Improve error handling in route.ts

--- src/app/[slug]/route.ts
+++ src/app/[slug]/route.ts
@@ -5,19 +5,10 @@ export async function GET(req: Request) {
   const url = new URL(req.url);
   const slug = url.pathname.split("/").pop();
 
-  const pageRoutes: { [key: string]: string } = {
-    login: "/login",
-    "": "/",
-    links: "/links",
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  if ((slug as any) in pageRoutes) {
-    return NextResponse.redirect(
-      pageRoutes[slug as keyof typeof pageRoutes],
-      301
-    );
+  if (!slug) {
+    return NextResponse.json({ message: "Slug not found" }, { status: 400 });
   }
+
   const { data, error } = await supabase
     .from("links")
     .select("original_url")
@@ -25,9 +16,7 @@ export async function GET(req: Request) {
     .single();
 
   if (error || !data) {
-    return new Response(JSON.stringify({ message: "Link not found" }), {
-      status: 404,
-    });
+    return NextResponse.json({ message: "Link not found" }, { status: 404 });
   }
 
   return NextResponse.redirect(data.original_url, 301);
