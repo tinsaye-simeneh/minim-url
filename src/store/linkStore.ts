9cde83198e33f3767964ec6887cd608626152d13
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-11-20 10:50:54 +0300

    feat: Add Zustand store for managing link data

--- src/store/linkStore.ts
+++ src/store/linkStore.ts
@@ -1,1 +1,12 @@
+import { create } from "zustand";
+import { Link } from "../types/models";
+
+export const useLinkStore = create((set)) => ({
+    originalUrl:"",
+    shortUrl:"",
+    links: [] as Link[],
+    setOriginalUrl: (originalUrl: string) => set({ originalUrl }),
+    setShortUrl: (shortUrl: string) => set({ shortUrl }),
+    
+});
 
